c6d8cfc45ef0e6ced65d5d9c18410c61fcb85a29
DANA <dana.yohann@gmail.com>
2022-08-25 08:01:50 -0400

    ex11 done

--- ex11/d05/src/ex11/Controller/DefaultController.php
+++ ex11/d05/src/ex11/Controller/DefaultController.php
@@ -57,55 +57,70 @@ class DefaultController extends Controller
         $form->handleRequest($request);
         if ($form->isValid() && $form->isSubmitted()){
             if($form["filtrage"]->getData() == true){
-                
+                $sql = "SELECT bank_account.id, person_e11.username, person_e11.name, person_e11.email, person_e11.enable, person_e11.birthdate, person_e11.marital_statut, addresses.a_name, bank_account.amount FROM person_e11 LEFT JOIN bank_account ON person_e11.id=bank_account.bank_id LEFT JOIN addresses ON person_e11.id=addresses.addresses_id ORDER BY birthdate;";
+                $em = $this->get('doctrine.orm.default_entity_manager');
+                try {
+                    $statement = $em->getConnection()->prepare($sql);
+                    $statement->execute();
+                }
+                catch(\Exception $e){
+                    $error = "Error";
+                }
+                $table = [];
+                foreach($statement->fetchAll() as $t){
+                    array_push($table, [
+                        'username' => $t["username"],
+                        'name' => $t["name"],
+                        'email' => $t["email"],
+                        'enable' => $t["enable"],
+                        'birthdate' => $t["birthdate"],
+                        'marital_statut' => $t["marital_statut"],
+                        'amount' => $t["amount"],
+                        'bank_id' => $t["id"],
+                        'a_name' => $t["a_name"],
+                    ]);
+                }
+                return $this->render('ex11:Default:index.html.twig', [
+                    "error" => $error,
+                    "table" => $table,
+                    "form" => $form->createView()
+                ]);
             }
             else{
-
+                $sql = "SELECT bank_account.id, person_e11.username, person_e11.name, person_e11.email, person_e11.enable, person_e11.birthdate, person_e11.marital_statut, addresses.a_name, bank_account.amount FROM person_e11 LEFT JOIN bank_account ON person_e11.id=bank_account.bank_id LEFT JOIN addresses ON person_e11.id=addresses.addresses_id ORDER BY username;";
+                $em = $this->get('doctrine.orm.default_entity_manager');
+                try {
+                    $statement = $em->getConnection()->prepare($sql);
+                    $statement->execute();
+                }
+                catch(\Exception $e){
+                    $error = "Error";
+                }
+                $table = [];
+                foreach($statement->fetchAll() as $t){
+                    array_push($table, [
+                        'username' => $t["username"],
+                        'name' => $t["name"],
+                        'email' => $t["email"],
+                        'enable' => $t["enable"],
+                        'birthdate' => $t["birthdate"],
+                        'marital_statut' => $t["marital_statut"],
+                        'amount' => $t["amount"],
+                        'bank_id' => $t["id"],
+                        'a_name' => $t["a_name"],
+                    ]);
+                }
+                return $this->render('ex11:Default:index.html.twig', [
+                    "error" => $error,
+                    "table" => $table,
+                    "form" => $form->createView()
+                ]);
             }
         }
         return $this->render('ex11:Default:index.html.twig', [
             "error" => $error,
             "table" => $table,
             "form" => $form->createView()
-        ]);
-    }
-     /**
-     * @Route("/e11/0")
-     */
-    public function filterDate()
-    {
-        $error = "";
-        $sql = "SELECT person_e11.username, person_e11.name, person_e11.email, person_e11.enable, person_e11.birthdate, person_e11.marital_statut, addresses.name, bank_account.amount FROM person_e11 LEFT JOIN bank_account ON person_e11.id=bank_account.bank_id LEFT JOIN addresses ON person_e11.id=addresses.addresses_id;";
-        $em = $this->get('doctrine.orm.default_entity_manager');
-        try {
-            $statement = $em->getConnection()->prepare($sql);
-            $statement->execute();
-        }
-        catch(\Exception $e){
-            $error = "Error";
-        }
-        return $this->render('ex11:Default:index.html.twig', [
-            "error" => $error,
-        ]);
-    }
-
-     /**
-     * @Route("/e11/1")
-     */
-    public function filterName()
-    {
-        $error = "";
-        $sql = "SELECT person_e11.username, person_e11.name, person_e11.email, person_e11.enable, person_e11.birthdate, person_e11.marital_statut, addresses.name, bank_account.amount FROM person_e11 LEFT JOIN bank_account ON person_e11.id=bank_account.bank_id LEFT JOIN addresses ON person_e11.id=addresses.addresses_id;";
-        $em = $this->get('doctrine.orm.default_entity_manager');
-        try {
-            $statement = $em->getConnection()->prepare($sql);
-            $statement->execute();
-        }
-        catch(\Exception $e){
-            $error = "Error";
-        }
-        return $this->render('ex11:Default:index.html.twig', [
-            "error" => $error,
         ]);
     }
 }
